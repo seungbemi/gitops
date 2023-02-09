df31c03e80dc94c1acea825eb5bfc7a8ac6e3a5e
Seungbeom Han <seungbeom.han@gmail.com>
2023-02-09 02:33:38 +0200

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install cni calico
-helm template -n kube-system kube-network ./kubernetes/kube-system/kube-network | kubectl apply -f -
+helm template -n kube-system kube-network ./kubernetes/kube-system/kube-network | kubectl apply -n kube-system -f -
 
 # argocd install
-helm install -n cicd cicd ./kubernetes/cicd/argocd --create-namespace --dependency-update
+helm template -n cicd cicd ./kubernetes/cicd/argocd --create-namespace --dependency-update | kubectl apply -n cicd -f -
 
-bw get notes gitops-secrets | kubectl apply -f -
+bw get notes gitops-secrets | kubectl apply -n cicd -f -
 
