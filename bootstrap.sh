7f76434906678b31b415b95fbf43838af711af54
Seungbeom Han <seungbeom.han@gmail.com>
2023-02-12 02:55:05 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 helm template -n kube-system kube-network ./kubernetes/kube-system/kube-network | kubectl apply -n kube-system -f -
 
 # argocd install
-helm template -n cicd cicd ./kubernetes/cicd/argocd --create-namespace --dependency-update
+helm template -n cicd cicd ./kubernetes/cicd/argocd --create-namespace --dependency-update | kubectl apply -n cicd -f -
 
 bw get notes gitops-secrets | kubectl apply -n cicd -f -
 
