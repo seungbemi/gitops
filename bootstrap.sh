2a641f1cf87b74a1d68bbc33c0aef9f0cf2acbac
Seungbeom Han <seungbeom.han@gmail.com>
2026-09-24 12:25:03 +0300

    argocd: add SOPS secret rendering

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+SCRIPT_PATH=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+"$SCRIPT_PATH/scripts/sync-argocd-sops-key.sh"
 
 # install cni weave
 helm template -n kube-system kube-network ./kubernetes/kube-system/kube-network | kubectl apply -n kube-system -f -
@@ -7,4 +11,3 @@ helm template -n kube-system kube-network ./kubernetes/kube-system/kube-network 
 helm template -n cicd cicd ./kubernetes/cicd/argocd --create-namespace --dependency-update | kubectl apply -n cicd -f -
 
 bw get notes gitops-secrets | kubectl apply -n cicd -f -
-
